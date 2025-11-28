6dc2374eba2fba390eaf844ac3406ef72392f302
scaler-bhavya <bhavya.sachdeva@scaler.com>
2025-11-28 15:00:11 +0530

    Update version message in SimpleApp

--- Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
+++ Jenkins-demo-app/src/main/java/com/example/SimpleApp.java
@@ -3,7 +3,7 @@ package com.example;
 public class SimpleApp {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------");
-        System.out.println("Hello! This application was built via Jenkins.Version-4");
+        System.out.println("Hello! This application was built via Jenkins.Version-4.5");
         System.out.println("--------------------------------------------------");
     }
 }
